f5020058eadbfba177e6f5866a722bc8474f86e0
fujimaki3968 <haruki3968@gmail.com>
2020-08-28 00:09:36 +0900

    read 4.3.2

--- functions.py
+++ functions.py
@@ -1,10 +1,13 @@
 import numpy as np
 
+
 def sigmoid(x):
-    return 1/(1+np.exp(-x))
+    return 1 / (1 + np.exp(-x))
+
 
 def relu(x):
     return np.maximum(0, x)
+
 
 # p69
 def softmax(x):
@@ -15,3 +18,24 @@ def softmax(x):
     y = exp_x / sum_exp_x
     return y
 
+
+### chapter 4
+
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y - t) ** 2)
+
+
+def cross_entropy_error(y, t):
+    delta = 1e-7
+
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+    batch_size = y.shape[0]
+    # log(0)を避けるために微小な数値を入れる
+    return -np.sum(t * np.log(y + delta)) / batch_size
+
+
+def numerical_diff(f, x):
+    h = 1e-4
+    return (f(x + h) - f(x - h)) / (2 * h)
